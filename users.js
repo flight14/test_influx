d315c4202befdbba2a0137ec6fbc1b617600f8a7
flight9 <ealeaf@sohu.com>
2018-05-23 13:44:18 +0800

    add user fields

--- users.js
+++ users.js
@@ -1,23 +1,26 @@
 module.exports = [
   {
     id: '100',
-    name: 'bstwo',
+    realname: 'base_two',
     groups: ['sydc1','sydc2'],
     openid: 'oW6aH0ditEHnptRI058xBw0pOBzg',
     mobile: '13011112222',
+    active: true,
   },
   // {
     // id: '101',
-    // name: 'lmumu',
+    // realname: 'lmumu',
     // groups: ['sydc1','sydc2'],
     // openid: 'oW6aH0fY6upkzy6H9OA70WC3pclI',
     // mobile: '13012341234',
+    // active: true,
   // },
   // {
     // id: '103',
-    // name: 'zhai.shaobin',
+    // realname: 'zhai.shaobin',
     // groups: ['sydc1','sydc2'],
     // openid: 'oW6aH0fpnF_jr-82wvLAMf2QKcNg',
     // mobile: '13088881234',
+    // active: true,
   // },
 ];
